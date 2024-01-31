676c3f8e60cee61ab88560a1314dfad375118c98
kbkoz <kbkozlev@gmail.com>
2024-01-31 12:24:52 +0200

    changed compressPictures.py to use PIL instead of pngquant.exe; Files are going to be converted to JPEG instead of PNG, but this saves about 25% of the file size and completes 87% faster.

--- src/compressPictures.py
+++ src/compressPictures.py
@@ -1,15 +1,14 @@
 import os
 import shutil
-import subprocess
+from PIL import Image, UnidentifiedImageError
 
 
-def compress_pictures(input_folder: str) -> tuple[bool, str] | tuple[bool, None]:
+def compress_pictures(input_folder: str) -> tuple[bool, str]:
     """
-    Uses a binary of "pngquant" to compress the images
+    Uses PIL to compress the images
     :param input_folder:
     :return:
     """
-    pngquant_path = 'src/helper/pngquant/pngquant.exe'
 
     # Create output folder if it doesn't exist
     output_folder = 'files/4.finalPictures/'
@@ -19,21 +18,20 @@ def compress_pictures(input_folder: str) -> tuple[bool, str] | tuple[bool, None]
     files = os.listdir(input_folder)
     files.sort(key=lambda x: int(os.path.splitext(x)[0]))
 
-    try:
-        for filename in files:
-            input_path = os.path.join(input_folder, filename)
-            output_path = os.path.join(output_folder, filename)
+    for filename in files:
+        input_path = os.path.join(input_folder, filename)
+        output_path = os.path.join(output_folder, filename)
 
-            result = subprocess.run([pngquant_path, "--force", "--output", output_path, input_path])
-            if result.returncode != 0:
-                print(f"Failed to compress image '{filename}', copying original.")
-                shutil.copyfile(input_path, output_path)
-            else:
+        try:
+            with Image.open(input_path) as img:
+                img.convert("RGB").save(output_path, 'JPEG', optimize=True)
                 print(f"Image '{filename}' compressed.")
+        except UnidentifiedImageError as uie:
+            print(f"Failed to compress image '{filename}' due to UnidentifiedImageError: {uie}\nCopying original!")
+            shutil.copyfile(input_path, output_path)
+        except Exception as e:
+            print(f"Failed to compress image '{filename}' with an unexpected error: {e}\nCopying original!")
+            shutil.copyfile(input_path, output_path)
 
-        print(f'\nAll pictures have been compressed and saved to folder {output_folder}\n')
-        return True, output_folder
-
-    except Exception as e:
-        print(f"\nError during compression: {e}")
-        return False, None
+    print(f'\nAll pictures have been compressed and saved to folder {output_folder}\n')
+    return True, output_folder
