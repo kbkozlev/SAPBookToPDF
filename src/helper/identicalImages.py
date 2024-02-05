ab323f9dbc30f12eae869922b301685c825fcde5
kbkoz <kbkozlev@gmail.com>
2024-02-05 18:27:30 +0200

    simplified the function

--- src/helper/identicalImages.py
+++ src/helper/identicalImages.py
@@ -3,17 +3,14 @@ from PIL import Image
 
 def are_images_identical(image1: str, image2: str) -> bool:
     """
-    Takes in the last two images in the list and compares the hashes.
-    If they are the same returns true
+    Takes in the last two images in the list and compares the pixels.
+    If they are the same returns True
     :param image1:
     :param image2:
     :return:
     """
 
-    img1 = Image.open(image1)
-    img2 = Image.open(image2)
-
-    if img1 == img2:
+    if Image.open(image1) == Image.open(image2):
         return True
 
     else:
