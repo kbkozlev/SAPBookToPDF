2ab1e061450fe54c6966d22e43e3701fedbc6325
kbkoz <kbkozlev@gmail.com>
2024-02-05 20:53:12 +0200

    made sure not to compress or sharpen the cover as this destroys quality of the cover page

--- src/compressPictures.py
+++ src/compressPictures.py
@@ -5,26 +5,33 @@ from concurrent.futures import ProcessPoolExecutor
 from src.helper.colorPrinter import Color
 
 
-def __compress_picture(path_tuple: tuple):
+def __compress_picture(args: list):
     """
     Private function, handles the compression of the images
-    :param path_tuple: Tuple of the input and output paths
+    :param args: Tuple of the input and output paths
     :return:
     """
-    input_path, output_path = path_tuple
-    try:
-        with Image.open(input_path) as img:
-            img.convert("RGB").save(output_path, 'JPEG', optimize=True)
-            print(f"Image '{os.path.basename(input_path)}' compressed.")
-    except UnidentifiedImageError as uie:
-        print(f"""{Color.red(f"Failed to compress image '{os.path.basename(input_path)}' due to UnidentifiedImageError: {uie}")}""")
-        print(f"{Color.red('Copying original!')}")
+    input_path, output_path = args
 
+    if os.path.basename(input_path).endswith("00.png"):  # Skip if the name of the pic is 00.png - e.g. the cover
         shutil.copyfile(input_path, output_path)
-    except Exception as e:
-        print(f"""{Color.red(f"Failed to compress image '{os.path.basename(input_path)}' with an unexpected error: {e}")}""")
-        print(f"{Color.red(f'Copying original!')}")
-        shutil.copyfile(input_path, output_path)
+        print(f"Image '{os.path.basename(input_path)}' copied.")
+
+    else:
+        try:
+            with Image.open(input_path) as img:
+                img.convert("RGB").save(output_path, 'JPEG', optimize=True)
+                print(f"Image '{os.path.basename(input_path)}' compressed.")
+
+        except UnidentifiedImageError as uie:
+            print(f"""{Color.red(f"Failed to compress image '{os.path.basename(input_path)}' due to UnidentifiedImageError: {uie}")}""")
+            print(f"{Color.red('Copying original!')}")
+            shutil.copyfile(input_path, output_path)
+
+        except Exception as e:
+            print(f"""{Color.red(f"Failed to compress image '{os.path.basename(input_path)}' with an unexpected error: {e}")}""")
+            print(f"{Color.red(f'Copying original!')}")
+            shutil.copyfile(input_path, output_path)
 
 
 def compress_pictures(input_folder: str) -> tuple[bool, str]:
--- src/sharpenPictures.py
+++ src/sharpenPictures.py
@@ -6,38 +6,45 @@ from concurrent.futures import ProcessPoolExecutor
 from src.helper.colorPrinter import Color
 
 
-def __sharpen_image(args: tuple):
+def __sharpen_image(args: list) -> bool:
     """
     Private function, handles the sharpening of the images with a kernel.
     :param args: Tuple of input and output paths.
     :return:
     """
     input_path, output_path = args
-    try:
-        img = cv2.imread(input_path)
 
-        if img is not None:
-            kernel = np.array([[0, -0.5, 0],
-                               [-0.5, 3, -0.5],
-                               [0, -0.5, 0]])
-
-            sharpened = cv2.filter2D(img, -1, kernel)
-
-            success = cv2.imwrite(output_path, sharpened)
-            if not success:
-                # If unable to save, copy the original image instead
-                shutil.copyfile(input_path, output_path)
-                print(f"""\n{Color.red(f"Unable to save processed image '{os.path.basename(input_path)}'. Copied original instead.")}""")
-            else:
-                print(f"Image '{os.path.basename(input_path)}' sharpened.")
-        else:
-            raise FileNotFoundError(f"""\n{Color.red(f"Unable to read '{os.path.basename(input_path)}'. The image file may be missing or corrupted.")}""")
-
+    if os.path.basename(input_path).endswith("00.png"):  # Skip if the name of the pic is 00.png - e.g. the cover
+        shutil.copyfile(input_path, output_path)
+        print(f"Image '{os.path.basename(input_path)}' copied.")
         return True
 
-    except Exception as e:
-        print(f"\n{Color.red(f'Unexpected error: {e}')}")
-        return False
+    else:
+        try:
+            img = cv2.imread(input_path)
+
+            if img is not None:
+                kernel = np.array([[0, -0.5, 0],
+                                   [-0.5, 3, -0.5],
+                                   [0, -0.5, 0]])
+
+                sharpened = cv2.filter2D(img, -1, kernel)
+
+                success = cv2.imwrite(output_path, sharpened)
+                if not success:
+                    # If unable to save, copy the original image instead
+                    shutil.copyfile(input_path, output_path)
+                    print(f"""\n{Color.red(f"Unable to save processed image '{os.path.basename(input_path)}'. Copied original instead.")}""")
+                else:
+                    print(f"Image '{os.path.basename(input_path)}' sharpened.")
+            else:
+                raise FileNotFoundError(f"""\n{Color.red(f"Unable to read '{os.path.basename(input_path)}'. The image file may be missing or corrupted.")}""")
+
+            return True
+
+        except Exception as e:
+            print(f"\n{Color.red(f'Unexpected error: {e}')}")
+            return False
 
 
 def sharpen_images_in_folder(input_folder: str) -> tuple[bool, str] | tuple[bool, None]:
